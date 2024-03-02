075f116df41c5a28444e022f9a690e24a9498b78
muthu-rgb <muthushankar95@gmail.com>
2024-03-02 09:51:16 +0000

    Fourth commit

--- clientRouter.js
+++ clientRouter.js
@@ -10,11 +10,20 @@ const checkExpiry = async (req, res, next) => {
 
     const currentDate = new Date();
     const createdAt = new Date(client.createdAt);
-    const expiryDate = new Date(createdAt.setDate(createdAt.getDate() + 9));  // Add 9 days expiry
-    const expiryDate31 = new Date(createdAt.setDate(createdAt.getDate() + 31)); // Add 31 days expiry
-    const expiryDate90 = new Date(createdAt.setDate(createdAt.getDate() + 90)); // Add 90 days expiry
-    const expiryDate5 = new Date(createdAt.getTime() + 5 * 60 * 1000); // Add 5 minutes expiry
-    const expiryDate5sec = new Date(createdAt.getTime() + 5 * 1000); // Add 5 seconds expiry
+    const expiryDate = new Date(createdAt.setDate(createdAt.getDate() + 9));     // Add 9 days expiry
+
+    const expiryDate31 = new Date(createdAt.setDate(createdAt.getDate() + 31));  // Add 31 days expiry
+    const expiryDate90 = new Date(createdAt.setDate(createdAt.getDate() + 90));  // Add 90 days expiry
+    const expiryDateHours = new Date(createdAt.getTime() + 5 * 60 * 60 * 1000);  // Add 5 hours expiry
+    const expiryDateMinutes = new Date(createdAt.getTime() + 5 * 60 * 1000);     // Add 5 minutes expiry
+    const expiryDateSeconds = new Date(createdAt.getTime() + 5 * 1000);          // Add 5 seconds expiry
+
+    // Add 5 years expiry
+    
+    const expiryDates = new Date(createdAt);
+    expiryDates.setFullYear(expiryDates.getFullYear() + 5);
+    
+    // 
 
     if (currentDate > expiryDate) {
       return res.status(400).send({ error: "The client is expired" });
